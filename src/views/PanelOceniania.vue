1551be0a575617e83ba52d1ec3bd76ca6804b321
Paweł Mach <machpawel95@gmail.com>
2019-09-10 15:48:17 +0200

    Forked from my older repo

---
+++ src/views/PanelOceniania.vue
@@ -0,0 +1,16 @@
+<template>
+    <KlasyTabs/>
+</template>
+
+<script>
+    import KlasyTabs from '@/components/klasy/KlasyTabs.vue';
+    export default {
+        data () {
+            return {
+            };
+        },
+        components: {
+            KlasyTabs
+        }
+    };
+</script>
